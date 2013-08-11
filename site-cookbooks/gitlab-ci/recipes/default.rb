eccb0e4d1fe9e3d708b1e00fea6ec3c7eb29a49e
George Drummond <georgedrummond@gmail.com>
2013-08-11 09:19:24 +0100

    run gitlab ci

--- site-cookbooks/gitlab-ci/recipes/default.rb
+++ site-cookbooks/gitlab-ci/recipes/default.rb
@@ -21,10 +21,64 @@ pg_database 'gitlab_ci_production' do
   owner 'gitlab_ci'
 end
 
-# GitLab
+# GitLab CI
 
 git '/home/gitlab_ci/gitlab-ci' do
   repository 'git://github.com/gitlabhq/gitlab-ci.git'
+  branch '3-1-stable'
   user 'gitlab_ci'
-end 
+end
 
+template '/home/gitlab_ci/gitlab-ci/config/puma.rb' do
+  source 'gitlab-ci.puma.rb'
+  owner 'gitlab_ci'
+end
+
+template '/home/gitlab_ci/gitlab-ci/config/application.yml' do
+  source 'gitlab-ci.application.yml'
+  owner 'gitlab_ci'
+end
+
+template '/home/gitlab_ci/gitlab-ci/config/database.yml' do
+  source 'gitlab-ci.database.yml'
+  owner 'gitlab_ci'
+end
+
+%w{ log tmp tmp/pid tmp/sockets }.each do |dir|
+  directory "/home/gitlab_ci/gitlab-ci/#{dir}" do
+    mode 00755
+    recursive true
+    action :create
+    owner 'gitlab_ci'
+  end
+end
+
+# Run bundler
+
+execute 'bundle_gitlab_ci' do
+  user 'gitlab_ci'
+  cwd '/home/gitlab_ci/gitlab-ci'
+  command 'bundle install --deployment --without development test mysql unicorn aws'
+end
+
+# Setup the database
+
+execute 'create_gitlab_ci_database' do
+  user 'gitlab_ci'
+  cwd '/home/gitlab_ci/gitlab-ci'
+  command 'bundle exec rake db:setup RAILS_ENV=production'
+end
+
+# init.d
+
+file '/home/gitlab_ci/gitlab-ci/lib/support/init.d/gitlab_ci' do
+  mode 00755
+end
+
+link '/etc/init.d/gitlab_ci' do
+  to '/home/gitlab_ci/gitlab-ci/lib/support/init.d/gitlab_ci'
+end
+
+service 'gitlab_ci' do
+  action [:enable, :start]
+end
